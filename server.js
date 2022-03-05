2ed15f17550a7766772305260a5bb420a133744e
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-03-05 19:08:39 +0600

    Lecture-5 done where I have implemented get and basic post method

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req,res) =>{
+    res.send("Express Home page!");
+})
+get.get('/api/students',(req,res) =>{
+    res.send("Get Data!");
+})
+
+get.post('/api/students',(req,res) =>{
+    res.send("Data Posted!");
+})
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
     console.log(`Server is running on port @${PORT}.`)
