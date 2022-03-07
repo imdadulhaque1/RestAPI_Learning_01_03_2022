d5223a542f43b34552b8bb90103d19d08fbf1c14
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-03-07 14:06:58 +0600

    Created new Route and connected with server

---
+++ api/routes/contacts.js
@@ -0,0 +1,10 @@
+const express = require("express");
+// const express = require('express').Router();
+const router = express.Router();
+
+
+router.get('/', (req, res, next) =>{
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const app = express();
 
+const contactRoute = require('./api/routes/contacts.js');
+
+
+// ------------> Routing Starting
+app.use('/api/contacts', contactRoute)
+// ------------> Routing Ended
+
 app.get('/', (req,res) =>{
     res.send("Express Home page!");
 })
-get.get('/api/students',(req,res) =>{
-    res.send("Get Data!");
+
+app.get('/post', (req, res) =>{
+    res.send("Posted Here!")
 })
 
-get.post('/api/students',(req,res) =>{
-    res.send("Data Posted!");
+app.get('/api/contacts', (req, res) =>{
+    res.json(contacts);
 })
 
+app.post('/api/contacts', (req, res) =>{
+    res.send({
+        "message": "Hello, Mr. Imdadul Haque. Have a message for you."
+    })
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
     console.log(`Server is running on port @${PORT}.`)
 })
+
+
+const contacts = [
+    {name: "Imdadul Haque", email:"imdad@gmail.com"},
+    {name: "Israt Jahan Maisha", email:"maisha@gmail.com"},
+    {name: "Shampa Khatun", email:"shampa@gmail.com"}
+]
