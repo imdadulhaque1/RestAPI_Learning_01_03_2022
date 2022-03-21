c183eaf26ede4e074972d9ce2b6f65f06543babe
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-03-21 15:35:49 +0600

    Route Contacts file cleared!

--- api/routes/contacts.js
+++ api/routes/contacts.js
@@ -11,26 +11,4 @@ router.get('/', contactController.getAllContactsController)
 router.post('/', contactController.postNewContactController )
 
 
-router.get('/:id', (req, res, next) =>{
-    res.json({
-        message: "I am Get Route."
-    })
-})
-
-router.post('/:id', (req, res, next) =>{
-    res.json({
-        message: "I am Post Route."
-    })
-})
-router.put('/:id', (req, res, next) =>{
-    res.json({
-        message: "I am Put Route."
-    })
-})
-router.delete('/:id', (req, res, next) =>{
-    res.json({
-        message: "I am Delete Route."
-    })
-})
-
 module.exports = router;
